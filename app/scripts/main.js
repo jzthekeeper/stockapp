26b477d135a6b9eb12a6634163edc8cfcfd93986
jz <jzthekeeper@126.com>
2015-01-28 00:05:28 +0800

    add: marionette framework

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,33 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        bootstrap: '../bower_components/bootstrap/dist/js/bootstrap'
+  paths: {
+    underscore: '../bower_components/underscore/underscore',
+    backbone: '../bower_components/backbone/backbone',
+    marionette: '../bower_components/backbone.marionette/lib/backbone.marionette',
+    jquery: '../bower_components/jquery/dist/jquery',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap'
+  },
+  shim: {
+    underscore: {
+      exports: '_'
     },
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
+    backbone: {
+      exports: 'Backbone',
+      deps: ['jquery', 'underscore']
+    },
+    marionette: {
+      exports: 'Backbone.Marionette',
+      deps: ['backbone']
+    },
+    bootstrap: {
+      deps: ['jquery'],
+      exports: 'jquery'
     }
+  }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+require(['app', 'jquery', 'backbone', 'marionette', 'bootstrap'], function (app, $, Backbone, Marionette) {//fix: bootstrap export $, but in arguments is nothing, put it as last argument
+  'use strict';
+  // use app here
+  console.log(app);
+  console.log('Running jQuery %s', $().jquery);
 });
