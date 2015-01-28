92358a023c022e0dc4cd04598d4d3ab938d4aa5b
jiwang <jiwang@alibaba-inc.com>
2015-01-28 19:18:11 +0800

    login modal TODO

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,8 @@ require.config({
     backbone: '../bower_components/backbone/backbone',
     marionette: '../bower_components/backbone.marionette/lib/backbone.marionette',
     jquery: '../bower_components/jquery/dist/jquery',
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap'
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+    text: '../bower_components/requirejs-text/text',
   },
   shim: {
     underscore: {
@@ -25,9 +26,9 @@ require.config({
   }
 });
 
-require(['app', 'jquery', 'backbone', 'marionette', 'bootstrap'], function (app, $, Backbone, Marionette) {//fix: bootstrap export $, but in arguments is nothing, put it as last argument
-  'use strict';
-  // use app here
-  console.log(app);
-  console.log('Running jQuery %s', $().jquery);
+require(['app', 'jquery', 'backbone', 'marionette', 'bootstrap'], 
+    function (app, $, Backbone, Marionette) {//fix: bootstrap export $, but in arguments is nothing, put it as last argument
+        'use strict';
+
+
 });
