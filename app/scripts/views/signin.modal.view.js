befb0680f74bc661778be1bf91f6688e0cac50eb
jz <jzthekeeper@126.com>
2015-01-31 00:52:25 +0800

    sign in and sign up modal all work

--- app/scripts/views/signin.modal.view.js
+++ app/scripts/views/signin.modal.view.js
@@ -2,17 +2,36 @@ define([
   '../templates',
   '../app',
   './signup.modal.view',
+  '../models/user.model',
+  'jquery',
   'backbone',
   'marionette',
   'backbone.modal'],
-  function (templates, app, SignUpModalView, Backbone) {
+  function (templates, app, SignUpModalView, UserModel, $, Backbone) {
     return Backbone.Modal.extend({
-        template: _.template(templates.loginModalTpl),
-        events: {
-          'click .bbm-button': 'signUp'
-        },
-        signUp: function() {
-          app.modalRegion.show(new SignUpModalView());
-        }
+      template: _.template(templates.loginModalTpl),
+      events: {
+        'click .sign-up': 'signUp',
+        'click .sign-in': 'signIn',
+      },
+      ui: {
+        userName: '#user-login-name',
+        pswd: '#user-login-pswd'
+      },
+      signUp: function() {
+        app.modalRegion.show(new SignUpModalView());
+      },
+      signIn: function() {
+        var modal = this;
+        $.post('key/web/stock/login.json', {
+          name: $(this.ui.userName).val(),
+          pswd: $(this.ui.pswd).val()
+        }).then(function() {
+          alert('sign in ok!');  
+          modal.destroy();
+        }, function() {
+          alert('sign in failed');
+        });
+      }
     });
-});
+  });
--- app/scripts/views/signup.modal.view.js
+++ app/scripts/views/signup.modal.view.js
@@ -6,27 +6,34 @@ define([
   'jquery',
   'marionette',
   'backbone.modal'],
-   function (templates, app, UserModel, Backbone, $) {
-     return Backbone.Modal.extend({
-         template: _.template(templates.signUpModalTpl),
-         model: new UserModel(),
-         ui: {
-           userName: '#user-register-name',
-           pswd: '#user-register-pswd'
-         },
-         events: {
-           'click .bbm-button.submit': 'submitRegister'
-         },
-         submitRegister: function() {
-           this.triggerSubmit();
-         },
-         submit: function() {
-           this.model.save({
-             name: $(this.ui.userName).val(),
-             pswd: $(this.ui.pswd).val()
-           });
-         }
+  function (templates, app, UserModel, Backbone, $) {
+    return Backbone.Modal.extend({
+      template: _.template(templates.signUpModalTpl),
+      model: new UserModel(),
+      ui: {
+        userName: '#user-register-name',
+        pswd: '#user-register-pswd'
+      },
+      events: {
+        'click .bbm-button.submit': 'submitRegister'
+      },
+      submitRegister: function() {
+        var view = this;
+        submit().then(function() {
+          alert('register succeed!');
+        }, function() {
+          alert('submit failed!');
+        });
+        function submit() {
+          return view.model.save({
+            name: $(view.ui.userName).val(),
+            pswd: $(view.ui.pswd).val()
+          });
+        }
+      },
+      submit: function() {
+      }
 
 
-     });
-   });
+    });
+  });
