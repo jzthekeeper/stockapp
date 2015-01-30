61fec0621b307e6d4f32f16d858e24df944655e1
jiwang <jiwang@alibaba-inc.com>
2015-01-30 17:35:03 +0800

    fix: jshintrc

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -1,7 +1,7 @@
-define(['../templates', '../views/signin.modal.view', '../app'], function (templates, signUpModalView, app) {
+define(['../templates', '../views/signin.modal.view', '../app'], function (templates, SignUpModalView, app) {
   return {
     openLoginModal: function() {
-      app.modalRegion.show(new signUpModalView());
+      app.modalRegion.show(new SignUpModalView());
     }  
   };
 });
